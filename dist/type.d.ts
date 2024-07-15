4f0ec6a0b86750d39857c408657fe0f5b6f50dac
TopTech126 <carlie19782@gmail.com>
2024-07-15 20:12:59 +1000

    init

---
+++ dist/type.d.ts
@@ -0,0 +1,17 @@
+import { SignTypedDataVersion } from '@metamask/eth-sig-util';
+import { Hex } from '@metamask/utils';
+export declare const SESSION_STATUS_MAP: {
+    CONNECTED: string;
+    DISCONNECTED: string;
+    ACCOUNT_ERROR: string;
+    REJECTED: string;
+    ADDRESS_DUPLICATE: string;
+    ACCOUNT_CHANGED: string;
+};
+export declare type KeyringOptions = {
+    accounts?: Hex[];
+};
+export declare type KeyringOpt = {
+    withAppKeyOrigin?: string;
+    version?: SignTypedDataVersion | string;
+};
---
+++ dist/type.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SESSION_STATUS_MAP = void 0;
+exports.SESSION_STATUS_MAP = {
+    CONNECTED: 'CONNECTED',
+    DISCONNECTED: 'DISCONNECTED',
+    ACCOUNT_ERROR: 'ACCOUNT_ERROR',
+    REJECTED: 'REJECTED',
+    ADDRESS_DUPLICATE: 'ADDRESS_DUPLICATE',
+    ACCOUNT_CHANGED: 'ACCOUNT_CHANGED',
+};
+//# sourceMappingURL=type.js.map
